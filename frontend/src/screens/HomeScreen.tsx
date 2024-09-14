35c5a8531e32f76536777e0956fe9a6a9608d5c0
sarahbicknell <166096006+sarahbicknell@users.noreply.github.com>
2024-09-14 02:47:25 -0400

    adds tooltip to homescreen

--- frontend/src/screens/HomeScreen.tsx
+++ frontend/src/screens/HomeScreen.tsx
@@ -1,8 +1,17 @@
 import { useNavigate } from 'react-router-dom';
 import './../index.css';
+import { CircleHelp } from "lucide-react";
+import { Tooltip } from 'react-tooltip';
 
 function HomeScreen() {
   const navigate = useNavigate();
+
+  const tooltipContent = `
+    <div>
+      <p style="font-size: 24px;">What's this?</p>
+      <p>A simple app that allows you to coordinate non-verbally and semi-anonymously with your friends. You choose a goal or action, and each person can click the button when they're ready to do it. Then the app will notify everyone when you've all clicked!</p>
+    </div>
+  `;
 
   return (
     <div className="flex flex-col justify-center items-center text-2xl text-center bg-[#fff9e6] w-full h-screen gap-4">
@@ -20,7 +29,23 @@ function HomeScreen() {
         >
           Join session
         </button>
+        <CircleHelp 
+          size="30"
+          className="ml-2 cursor-pointer"
+          data-tooltip-id="home-tooltip" 
+          data-tooltip-html={tooltipContent}
+        />
       </div>
+      <Tooltip 
+        id="home-tooltip" 
+        place="bottom"
+        style={{ 
+          maxWidth: '90vw', 
+          wordWrap: 'break-word',
+          fontSize: '20px',
+          zIndex: 1000
+        }}
+      />
     </div>
   );
 }
